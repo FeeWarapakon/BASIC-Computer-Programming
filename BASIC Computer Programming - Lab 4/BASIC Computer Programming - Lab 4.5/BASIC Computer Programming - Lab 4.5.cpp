cec2411412b15c7c823f5cc6ae2d890a6a69680f
Fee <feenix7248@gmail.com>
2025-12-23 21:45:46 +0700

    Update 23/12/68

--- BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.5/BASIC Computer Programming - Lab 4.5.cpp
+++ BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.5/BASIC Computer Programming - Lab 4.5.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
-
-struct Product {
-    char name[50];
-    int productId;
-    float price;
-};
+#define SIZE 6
 
 int main() {
-    struct Product item;
+    int data[SIZE];
+    int total_sum = 0;
+    int i;
 
     printf("Enter Input : ");
-    if (scanf("%d %f %s", &item.productId, &item.price, item.name) != 3) {
-        return 1;
+    for (i = 0; i < SIZE; i++) {
+        if (scanf("%d", &data[i]) != 1) {
+            return 1;
+        }
     }
-    printf("ID: %d, Price: %.2f, Name: %s\n", item.productId, item.price, item.name);
+    for (i = 0; i < SIZE; i++) {
+        total_sum += data[i];
+    }
+    printf("\n--- SUMMATION REPORT ---\n");
+    printf("Recorded Numbers: ");
+    for (i = 0; i < SIZE; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    printf("Total Sum of Numbers: %d\n", total_sum);
 
     return 0;
-
-}//end function 
+}//end function
