b6009739ea12f14b4d057e03dd326ea3d6e1aea4
Fee <feenix7248@gmail.com>
2025-12-23 21:34:28 +0700

    Update 23/12/68

--- BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.3/BASIC Computer Programming - Lab 4.3.cpp
+++ BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.3/BASIC Computer Programming - Lab 4.3.cpp
@@ -1,20 +1,35 @@
 #include <stdio.h>
-
-struct Product {
-    char name[50];
-    int productId;
-    float price;
-};
+#define DAYS 7
 
 int main() {
-    struct Product item;
+    int daily_temp[DAYS];
+    int max_temp;
+    int i;
 
     printf("Enter Input : ");
-    if (scanf("%d %f %s", &item.productId, &item.price, item.name) != 3) {
-        return 1;
+    for (i = 0; i < DAYS; i++) {
+        if (scanf("%d", &daily_temp[i]) != 1) {
+            return 1;
+        }
     }
-    printf("ID: %d, Price: %.2f, Name: %s\n", item.productId, item.price, item.name);
+    max_temp = daily_temp[0]; 
+    for (i = 1; i < DAYS; i++) {
+        if (daily_temp[i] > max_temp) {
+            max_temp = daily_temp[i];
+        }
+    }
+    printf("\n--- DAILY TEMPERATURE REPORT ---\n");
+    printf("Recorded Temperatures (C): ");
+    for (i = 0; i < DAYS; i++) {
+        printf("%d ", daily_temp[i]);
+    }
+    printf("\n");
+    printf("Maximum Temperature Found: %d C\n", max_temp);
+    if (max_temp >= 35) {
+        printf("Weather is HOT!\n");
+    } else {
+        printf("Weather is MODERATE.\n");
+    }
 
     return 0;
-
-}//end function 
+}//end function
