be5ef8e17515fac6cc2d4c6be6b961349d5c662e
Fee <feenix7248@gmail.com>
2025-12-23 21:51:35 +0700

    Update 23/12/68

--- BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.6/BASIC Computer Programming - Lab 4.6.cpp
+++ BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.6/BASIC Computer Programming - Lab 4.6.cpp
@@ -1,20 +1,35 @@
 #include <stdio.h>
-
-struct Product {
-    char name[50];
-    int productId;
-    float price;
-};
+#define STUDENTS 5
 
 int main() {
-    struct Product item;
+    int scores[STUDENTS];
+    int sum = 0;
+    float average = 0.0;
+    int above_average_count = 0;
+    int i;
 
     printf("Enter Input : ");
-    if (scanf("%d %f %s", &item.productId, &item.price, item.name) != 3) {
-        return 1;
+    for (i = 0; i < STUDENTS; i++) {
+        if (scanf("%d", &scores[i]) != 1) {
+            return 1;
+        }
+        sum += scores[i];
     }
-    printf("ID: %d, Price: %.2f, Name: %s\n", item.productId, item.price, item.name);
+    average = (float)sum / STUDENTS;
+    for (i = 0; i < STUDENTS; i++) {
+        if (scores[i] > average) {
+            above_average_count++;
+        }
+    }
+    printf("\n--- STUDENT SCORE ANALYSIS REPORT ---\n");
+    printf("Recorded Scores: ");
+    for (i = 0; i < STUDENTS; i++) {
+        printf("%d ", scores[i]);
+    }
+    printf("\n");
+    printf("Total Sum: %d\n", sum);
+    printf("Average Score: %.2f\n", average);
+    printf("Number of Scores Above Average: %d\n", above_average_count);
 
     return 0;
-
-}//end function 
+}//end function
