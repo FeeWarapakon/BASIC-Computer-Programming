e58a722d04a1ca1474b8207d90f80aa4e0e3b008
Fee <feenix7248@gmail.com>
2025-12-23 22:17:13 +0700

    Update 23/12/68

--- BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.11/BASIC Computer Programming - Lab 4.11.cpp
+++ BASIC Computer Programming - Lab 4/BASIC Computer Programming - Lab 4.11/BASIC Computer Programming - Lab 4.11.cpp
@@ -1,20 +1,36 @@
 #include <stdio.h>
-
-struct Product {
-    char name[50];
-    int productId;
-    float price;
-};
+#define ROWS 2
+#define COLS 4
 
 int main() {
-    struct Product item;
+    int matrix[ROWS][COLS];
+    int total_sum = 0;
+    int i, j;
 
     printf("Enter Input : ");
-    if (scanf("%d %f %s", &item.productId, &item.price, item.name) != 3) {
-        return 1;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                return 1;
+            }
+        }
     }
-    printf("ID: %d, Price: %.2f, Name: %s\n", item.productId, item.price, item.name);
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            total_sum += matrix[i][j];
+        }
+    }
+    printf("\n--- 2D ARRAY SUMMATION REPORT ---\n");
+    printf("Recorded Matrix (2x4):\n");
+
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nTotal Sum of all elements: %d\n", total_sum);
 
     return 0;
-
-}//end function 
+}//end function
